91298abf8742b5f676fe734837f97a1706537e5f
12482 <syf1248226176@gmail.com>
2026-01-17 18:16:17 -0600

    update user search function

---
+++ GreenNote-search/src/main/java/com/evan/greennote/search/config/ElasticsearchProperties.java
@@ -0,0 +1,13 @@
+package com.evan.greennote.search.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+//Elasticsearch 配置项
+@ConfigurationProperties(prefix = "elasticsearch")
+@Component
+@Data
+public class ElasticsearchProperties {
+    private String address;
+}
